46dd0f3004f0c2f6a25fef79c473e0b7634d44bd
ilias.otsman.7e4 <ilias.otsman.7e4@itb.cat>
2023-04-11 15:51:31 +0200

    year filter added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,10 +14,13 @@ export class HomePage {
   apiURL = "https://apibikes.up.railway.app/apiBikes";
   bikes = <any>[];
   bikesCount = 0;
+  
   brands = <any>[];
+  years = <any>[];
 
   filtredBrand = "all";
   filtredOrder = "relevance"
+  filtredYear = "any"
 
   constructor() {}
 
@@ -34,24 +37,44 @@ export class HomePage {
       if(!this.brands.includes(bike.brand)){
         this.brands.push(bike.brand);
       }
+      if(!this.years.includes(bike.year)){
+        this.years.push(bike.year);
+      }
+      this.years.sort(function(a: number, b: number){return b-a});
     });
   }
 
   async getBikesFiltred(){
     this.bikesCount = 0;
-    if(this.filtredBrand == "all" && this.filtredOrder == "relevance"){
+    if(this.filtredBrand == "all" && this.filtredOrder == "relevance" && this.filtredYear == "any"){
       this.getBikes();
     }
-    if(this.filtredBrand != "all" && this.filtredOrder == "relevance"){
+    if(this.filtredBrand != "all" && this.filtredOrder == "relevance" && this.filtredYear == "any"){
       const jsonBikes = await fetch(this.apiURL + "/motos?brand=" + this.filtredBrand);
       this.bikes = await jsonBikes.json();
     }
-    if(this.filtredBrand == "all" && this.filtredOrder != "relevance"){
+    if(this.filtredBrand == "all" && this.filtredOrder != "relevance" && this.filtredYear == "any"){
       const jsonBikes = await fetch(this.apiURL + "/motos?order=" + this.filtredOrder);
       this.bikes = await jsonBikes.json();
     }
-    if(this.filtredBrand != "all" && this.filtredOrder != "relevance"){
+    if(this.filtredBrand == "all" && this.filtredOrder == "relevance" && this.filtredYear != "any"){
+      const jsonBikes = await fetch(this.apiURL + "/motos?year=" + this.filtredYear);
+      this.bikes = await jsonBikes.json();
+    }
+    if(this.filtredBrand != "all" && this.filtredOrder != "relevance" && this.filtredYear == "any"){
       const jsonBikes = await fetch(this.apiURL + "/motos?order=" + this.filtredOrder + "&brand=" + this.filtredBrand);
+      this.bikes = await jsonBikes.json();
+    }
+    if(this.filtredBrand != "all" && this.filtredOrder == "relevance" && this.filtredYear != "any"){
+      const jsonBikes = await fetch(this.apiURL + "/motos?year=" + this.filtredYear + "&brand=" + this.filtredBrand);
+      this.bikes = await jsonBikes.json();
+    }
+    if(this.filtredBrand == "all" && this.filtredOrder != "relevance" && this.filtredYear != "any"){
+      const jsonBikes = await fetch(this.apiURL + "/motos?order=" + this.filtredOrder + "&year=" + this.filtredBrand);
+      this.bikes = await jsonBikes.json();
+    }
+    if(this.filtredBrand != "all" && this.filtredOrder != "relevance" && this.filtredYear != "all"){
+      const jsonBikes = await fetch(this.apiURL + "/motos?order=" + this.filtredOrder + "&brand=" + this.filtredBrand + "&year=" + this.filtredYear);
       this.bikes = await jsonBikes.json();
     }
     this.bikes.forEach((bike: any) => {
@@ -64,5 +87,9 @@ export class HomePage {
   }
   orderFilter(ev: any){
     this.filtredOrder = ev.target.value;
+  }
+  yearFilter(ev: any){
+    this.filtredYear = ev.target.value;
+    console.log(this.filtredYear);
   }
 }
