8e161a167564a27d4f9ae517e6ae95fa72b6b33d
ilias.otsman.7e4 <ilias.otsman.7e4@itb.cat>
2023-04-22 14:23:44 +0200

    addbikes

--- src/app/pages/add/add.page.ts
+++ src/app/pages/add/add.page.ts
@@ -1,42 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonicModule, NavController } from '@ionic/angular';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 
 @Component({
-  selector: 'app-add',
-  templateUrl: './add.page.html',
-  styleUrls: ['./add.page.scss'],
-  standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, HttpClientModule]
+selector: 'app-add',
+templateUrl: './add.page.html',
+styleUrls: ['./add.page.scss'],
+standalone: true,
+imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule]
 })
 export class AddPage implements OnInit {
 
-  apiURL = "https://apibikes.up.railway.app/apiBikes";
+apiURL = "https://apibikes.up.railway.app/apiBikes";
+addBikeForm!: FormGroup;
 
-  brand = new FormControl();
-  model = new FormControl();
-  year = new FormControl();
-  price = new FormControl();
+constructor(private navCtrl: NavController, private http: HttpClient) { }
 
-  constructor(private navCtrl: NavController, private http: HttpClient) { }
+ngOnInit() {
+this.addBikeForm = new FormGroup({
+image: new FormControl(null, [Validators.required]),
+brand: new FormControl(null, [Validators.required, Validators.minLength(2)]),
+model: new FormControl(null, [Validators.required, Validators.minLength(2)]),
+year: new FormControl(null, [Validators.required, Validators.min(2000)]),
+price: new FormControl(null, [Validators.required, Validators.min(350)])
+});
+}
 
-  ngOnInit() {}
+submitAddBike(){
+let headers = new HttpHeaders({
+brand: this.addBikeForm.value.brand,
+model: this.addBikeForm.value.model,
+year: this.addBikeForm.value.year.toString(),
+price: this.addBikeForm.value.price.toString(),
+image: this.addBikeForm.value.image
+});
+this.http.post<any>(`${this.apiURL}/moto`, null, {headers}).subscribe(res => {
+console.log(res);
+});
+this.goBackHome(true);
+}
 
-  addBike(){
-    console.log(1);
-    /* this.http.post(`${this.apiURL}/moto`, { Headers: {
-      brand: this.brand,
-      model: this.model,
-      year: this.year,
-      price: this.price
-    }}).subscribe(); */
-    this.goBackHome(true);
-  }
-
-  goBackHome(detailAction: boolean) {
-    this.navCtrl.navigateBack(['home', { detailAction }]);
-  }
+goBackHome(detailAction: boolean) {
+this.navCtrl.navigateBack(['home', { detailAction }]);
+}
 
 }
+
